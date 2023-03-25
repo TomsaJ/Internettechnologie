39aa934408deddaa84fe253af8f63aba01d1b188
TomsaJ <julian.tomsa@gmail.com>
2023-03-25 23:53:58 +0100

    Initial commit

---
+++ Blatt1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Meine Seite</title>
+	</head>
+	<body>
+		<h1>Erste Seite!!!</h1>
+		<div> Hello World!</div>
+		<div>
+			<a href="newfile.php" style="text-decoration:none">
+    			<span class="textstyle8">  Andere Seite</span>
+			</a>
+		</div>
+	</body>
+</html>
+
+
---
+++ Blatt1/newfile.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head >
+		<title>Zweite Seite</title>
+		<link rel="stylesheet" type="text/css" href="styles.css">
+	</head>
+	<body>
+		<h1>Zweite Seite</h1>
+		<div> Das ist eine andere Seite</div>
+		<a href="index.php" style="text-decoration:none">
+    		  <span class="textstyle8">  Back zur ersten Seite</span>
+    	</a>
+	</body>
+</html>
