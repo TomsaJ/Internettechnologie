8be79c7710d05ac6c74c8eec1e22f24789b07823
TomsaJ <julian.tomsa@gmail.com>
2023-03-31 10:30:24 +0200

    add Blatt2

---
+++ Blatt2/tabelle.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Beispiel Tabelle</title>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<table>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Alter</th>
+				<th>Stadt</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Max Mustermann</td>
+				<td>25</td>
+				<td>Berlin</td>
+			</tr>
+			<tr>
+				<td>Anna Müller</td>
+				<td>32</td>
+				<td>Hamburg</td>
+			</tr>
+			<tr>
+				<td>Peter Schmidt</td>
+				<td>41</td>
+				<td>München</td>
+			</tr>
+		</tbody>
+	</table>
+
+</body>
+</html>
